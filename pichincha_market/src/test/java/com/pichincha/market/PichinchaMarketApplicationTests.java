e267158096346cc7fb0f0dafb054102bc4f6692e
Andrés Dávila Bastidas <andres_adb@live.com>
2021-10-23 11:03:01 -0500

    Commit Final

---
+++ pichincha_market/src/test/java/com/pichincha/market/PichinchaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PichinchaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
